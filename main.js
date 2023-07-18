cb005a7c0019ed7ddcf9d1bdd05e34a75c9e506c
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-07-18 12:28:01 +0200

    handle ip sync fails

--- main.js
+++ main.js
@@ -8,12 +8,16 @@ while (true) {
     let txt = await (await fetch(file)).text();
     if (txt != "online") throw new Error("Repeat");
   } catch {
-    http.get({ host: "api.ipify.org", port: 80, path: "/" }, function (resp) {
-      resp.on("data", function (ip) {
-        console.log("update", update.replace("<ipaddr>", ip));
-        fetch(update.replace("<ipaddr>", ip));
+    try {
+      http.get({ host: "api.ipify.org", port: 80, path: "/" }, function (resp) {
+        resp.on("data", function (ip) {
+          console.log("update", update.replace("<ipaddr>", ip));
+          fetch(update.replace("<ipaddr>", ip));
+        });
       });
-    });
+    } catch (e) {
+      console.log("IP sync fail");
+    }
   }
 
   await new Promise((r) => setTimeout(r, 1000));
